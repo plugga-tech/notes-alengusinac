ff46080ad2aa9bc3c042c6a48c89c7f2fe2ef665
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-04-03 16:17:21 +0200

    added user login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,25 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', (req, res) => {
+  console.log(req.body);
+
+  database.connect((err) => {
+    if (err) {
+      console.error('conError', err);
+    }
+    const sql = `SELECT id FROM users WHERE username LIKE '${req.body.username}' AND password LIKE '${req.body.password}' LIMIT 1`;
+
+    database.query(sql, (err, result) => {
+      if (err) {
+        console.error('queryError', err);
+      }
+      console.log('RESULT', result);
+      res.json(result);
+    });
+  });
+});
+
 router.post('/create', (req, res) => {
   console.log(req.body);
 
--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -2,20 +2,32 @@ import '../style.css';
 
 const app = document.querySelector('#app');
 const BASE_URL = 'http://localhost:3000';
+let user = JSON.parse(localStorage.getItem('user'));
 
 tinymce.init({
   selector: '#myTextArea',
 });
 
+function checkLogin() {
+  user = JSON.parse(localStorage.getItem('user'));
+
+  if (user) {
+    printDocuments();
+  } else {
+    printLogin();
+  }
+}
 function printLogin() {
   app.innerHTML = `
     <h4>Login:</h4>
+    <div id="loginMessage"></div>
     <form>
       <input type="text" id="loginUsername" placeholder="Username">
       <input type="password" id="loginPassword" placeholder="Password">
       <button id="loginBtn">Login</button>
     </form>
     <h4>Create user:</h4>
+    <div id="createUserMessage"></div>
     <form>
       <input type="text" id="createUserUsername" placeholder="Username">
       <input type="password" id="createUserPassword" placeholder="Password">
@@ -30,10 +42,39 @@ function printLogin() {
   createUserBtn.addEventListener('click', createUser);
 }
 
-async function login() {}
+async function login(e) {
+  e.preventDefault();
+
+  const username = document.querySelector('#loginUsername').value;
+  const password = document.querySelector('#loginPassword').value;
+
+  if (username && password) {
+    const user = { username, password };
+
+    fetch(BASE_URL + '/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.length > 0) {
+          localStorage.setItem('user', JSON.stringify(data[0]));
+          checkLogin();
+        } else {
+          document.querySelector('#loginMessage').innerHTML =
+            "Username and password don't match.";
+        }
+      });
+  }
+}
 
 async function createUser(e) {
   e.preventDefault();
+
   const username = document.querySelector('#createUserUsername').value;
   const password = document.querySelector('#createUserPassword').value;
 
@@ -48,12 +89,16 @@ async function createUser(e) {
       body: JSON.stringify(user),
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        document.querySelector('#createUserMessage').innerHTML =
+          'User created.';
+      });
   }
 }
 
 function init() {
-  printLogin();
+  checkLogin();
 }
 
 init();
