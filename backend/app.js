99e06cc877117d852d0bbc82739c9f2ab8b0558e
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-04-04 08:05:02 +0200

    added documents router get

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var documentsRouter = require('./routes/documents');
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/documents', documentsRouter);
 
 module.exports = app;
--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 const database = require('../database');
 
+router.get('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  console.log(userId);
+  database.connect((err) => {
+    if (err) {
+      console.error('conError', err);
+    }
+
+    const sql = `SELECT * FROM documents WHERE userId = ${userId}`;
+
+    database.query(sql, (err, result) => {
+      if (err) {
+        console.error('queryError', err);
+      }
+      console.log(result);
+      res.send(result);
+    });
+  });
+});
+
 router.post('/create', (req, res) => {
   console.log(req.body);
 
@@ -9,14 +29,14 @@ router.post('/create', (req, res) => {
     if (err) {
       console.error('conError', err);
     }
-    const sql = `INSERT INTO documents (title, description, value) VALUES ("${req.body.username}", "${req.body.password}")`;
+    const sql = `INSERT INTO documents (userID, title, description) VALUES ("${req.body.user.id}", "${req.body.title}", "${req.body.description}")`;
 
     database.query(sql, (err, result) => {
       if (err) {
         console.error('queryError', err);
       }
       console.log(result);
-      res.json(result);
+      res.json(result.insertId);
     });
   });
 });
